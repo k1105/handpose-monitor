3c97a17b772171abb24a0aa65d14b1ca1cfd647f
k1105 <kanapon1105@yahoo.co.jp>
2023-09-12 18:10:11 +0900

    記録機能と再生機能の追加

--- sketch/HandSketch.tsx
+++ sketch/HandSketch.tsx
@@ -15,19 +15,22 @@ import { resizeHandpose } from "../lib/converter/resizeHandpose";
 
 type Props = {
   handpose: MutableRefObject<Hand[]>;
+  isLost: MutableRefObject<boolean>;
 };
 const Sketch = dynamic(import("react-p5"), {
   loading: () => <></>,
   ssr: false,
 });
 
-export const HandSketch = ({ handpose }: Props) => {
+export const HandSketch = ({ handpose, isLost }: Props) => {
   const handposeHistory = new HandposeHistory();
   const displayHands = new DisplayHands();
   const recordedDataRef = useRef<{ left: number[]; right: number[] }[]>([]);
+  const archiveRef = useRef<{ left: number[]; right: number[] }[]>([]);
   const position = useRef<Keypoint>({ x: 0, y: 200 });
   const scale = useRef<number>(1);
   const offset = useRef<number>(0.2);
+  const timeRef = useRef<number>(0);
 
   const debugLog = useRef<{ label: string; value: any }[]>([]);
 
@@ -71,6 +74,99 @@ export const HandSketch = ({ handpose }: Props) => {
     p5.clear();
 
     displayHands.update(hands);
+    if (isLost.current) {
+      if (recordedDataRef.current.length > 10) {
+        //記録の終了
+        const content = JSON.stringify(recordedDataRef.current);
+        const blob = new Blob([content], { type: "text/plain" });
+        const objUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = objUrl;
+        link.download = String(Date.now()) + ".json";
+        link.click();
+        archiveRef.current = recordedDataRef.current;
+        recordedDataRef.current = [];
+      }
+    } else {
+      const leftKeypoints = [];
+      for (const keypoint of rawHands.left) {
+        leftKeypoints.push(keypoint.x, keypoint.y);
+      }
+      const rightKeypoints = [];
+      for (const keypoint of rawHands.right) {
+        rightKeypoints.push(keypoint.x, keypoint.y);
+      }
+      recordedDataRef.current.push({
+        left: leftKeypoints,
+        right: rightKeypoints,
+      });
+    }
+
+    if (isLost.current && archiveRef.current.length > 0) {
+      const rawPose = archiveRef.current[timeRef.current];
+      if (rawPose.left.length > 0) {
+        p5.push();
+        p5.stroke(255);
+        p5.fill(255);
+        p5.translate(
+          p5.width * (0.5 - offset.current),
+          p5.height / 2 + +position.current.y
+        );
+        const pose: Keypoint[] = [];
+        for (let i = 0; i < 21; i++) {
+          pose.push({ x: rawPose.left[2 * i], y: rawPose.left[2 * i + 1] });
+        }
+        lineHand({
+          p5,
+          hand: resizeHandpose(
+            pose,
+            Math.max(p5.width, p5.height) * 1.5 * scale.current
+          ),
+          strokeWeight: Math.max(p5.width, p5.height) / 300,
+        });
+        dotHand({
+          p5,
+          hand: resizeHandpose(
+            pose,
+            Math.max(p5.width, p5.height) * 1.5 * scale.current
+          ),
+          dotSize: Math.max(p5.width, p5.height) / 100,
+        });
+        p5.pop();
+      }
+      if (rawPose.right.length > 0) {
+        p5.push();
+        p5.stroke(255);
+        p5.fill(255);
+        p5.translate(
+          p5.width * (0.5 + offset.current),
+          p5.height / 2 + +position.current.y
+        );
+        const pose: Keypoint[] = [];
+        for (let i = 0; i < 21; i++) {
+          pose.push({ x: rawPose.right[2 * i], y: rawPose.right[2 * i + 1] });
+        }
+        lineHand({
+          p5,
+          hand: resizeHandpose(
+            pose,
+            Math.max(p5.width, p5.height) * 1.5 * scale.current
+          ),
+          strokeWeight: Math.max(p5.width, p5.height) / 300,
+        });
+        dotHand({
+          p5,
+          hand: resizeHandpose(
+            pose,
+            Math.max(p5.width, p5.height) * 1.5 * scale.current
+          ),
+          dotSize: Math.max(p5.width, p5.height) / 100,
+        });
+        p5.pop();
+      }
+
+      timeRef.current = (timeRef.current + 1) % archiveRef.current.length;
+    }
 
     if (displayHands.left.pose.length > 0) {
       p5.push();
